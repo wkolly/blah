66fc553cca0d5d7971f5f11b0545193eb6b5b935
William Koelliker <willkoelliker22@gmail.com>
2024-03-06 09:35:29 -0700

    college basketball teams

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CollegeBasketballTeams from "./CollegeBasketballTeams.json";
+
+interface Team {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to the NCAA Basketball Teams Website</h1>
+    </div>
+  );
+}
+
+function Teams({ team }: { team: Team }) {
+  return (
+    <div>
+      <h2>{team.school}</h2>
+      <p>Mascot: {team.name}</p>
+      <p>
+        Location: {team.city}, {team.state}
+      </p>
+    </div>
+  );
+}
+
+function TeamList() {
+  return (
+    <div>
+      {CollegeBasketballTeams.teams.map((team: Team, index: number) => (
+        <Teams key={index} team={team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
